9422ac1de5688f304303879d135a71859c64349e
Kartavchenko <stranger1301078@gmail.com>
2022-10-07 23:03:12 +0200

    maked Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+// import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+import { movieAPI } from '../service/movieAPI';
+import { Home } from '../pages/Home';
+import { Cast } from '../pages/Cast';
+import { MovieDetails } from '../pages/MovieDetails';
+import { Movies } from '../pages/Movies';
+import { Reviews } from '../pages/Reviews';
+
 export const App = () => {
+  // const [films, setFilms] = useState([]);
+  console.log(movieAPI());
+
+  // useEffect(() => {
+  //   const fetchFilms = async() => {
+  //     const data = await
+  //   }
+  // }, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/service/movieAPI.jsx
@@ -0,0 +1,14 @@
+export const movieAPI = async () => {
+  const BASE_URL = 'https://api.themoviedb.org/3';
+  const API_KEY = '88dd01fe4db3764066bc9e293cf17684';
+  const TRENDING = 'trending/all/day';
+  //   const SEARCH = 'search/movie';
+  //   const DETAILS = '/movie/{movie_id}';
+  //   const CREDITS = 'movie/{movie_id}/credits';
+  //   const REVIES = 'movie/{movie_id}/reviews';
+
+  const movies = await fetch(`${BASE_URL}/${TRENDING}?api_key=${API_KEY}`);
+  console.log(movies);
+  //   console.log(movies.json());
+  return await movies.json();
+};
